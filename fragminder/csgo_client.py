043a8b11f63b1ad168a8e77d0c91b0b7943a1a20
lq0000 <root@lo.calho.st>
2020-12-11 15:38:56 -0700

    Reconnect to GC automatically

--- fragminder/csgo_client.py
+++ fragminder/csgo_client.py
@@ -1,6 +1,7 @@
 from steam.client import SteamClient
 from csgo.client import CSGOClient
-from csgo.enums import ECsgoGCMsg
+from csgo.enums import ECsgoGCMsg, GCConnectionStatus
+import logging
 
 __all__ = ['csgo_client']
 
@@ -12,19 +13,32 @@ class csgo_client (object):
 
         self._ready = False
 
+        @self._client.on("channel_secured")
+        def send_login():
+            if self._client.relogin_available:
+                self._client.relogin()
+
+        @self._client.on("disconnected")
+        def handle_disconnect():
+            logging.warning("disconnected from steam")
+            if self._client.relogin_available:
+                self._client.reconnect(maxdelay=10)
+
         @self._client.on('logged_on')
         def start_csgo():
+            logging.info("connecting to csgo")
             self._cs.launch()
 
         @self._cs.on('ready')
         def gc_ready():
+            logging.info("connected")
             self._ready = True
 
         self._client.cli_login(username=config["steam_bot_username"], password=config["steam_bot_password"])
 
         while not self._ready:
-            self._client.sleep(0.125)
-    
+            self._client.sleep(0.125)        
+
     def send(self, *args, **kwargs):
         self._cs.send(*args, **kwargs)
 
@@ -32,6 +46,10 @@ class csgo_client (object):
         return self._cs.wait_event(*args, **kwargs)
 
     def get_item_killcount(self, s, a, d):
+        if not self._cs.connection_status == GCConnectionStatus.HAVE_SESSION:
+            logging.warning("not connected to GC")
+            raise ValueError("not connected to gc")
+
         self.send(ECsgoGCMsg.EMsgGCCStrike15_v2_Client2GCEconPreviewDataBlockRequest, {
             'param_s': s,
             'param_a': a,
@@ -40,5 +58,5 @@ class csgo_client (object):
         })
         response, = self.wait_event(ECsgoGCMsg.EMsgGCCStrike15_v2_Client2GCEconPreviewDataBlockResponse, timeout=2)
         if response.iteminfo.itemid != a:
-            raise ValueError("mismatched req/rep, pls retry")
+            raise ValueError("mismatched req/rep")
         return response.iteminfo.killeatervalue
