f49027432c1e20b9ace7512a134b29655f1c30b3
Edwin Pers <pers.edwin@gmail.com>
2023-03-16 19:52:02 +0000

    dockerization changes/fixes

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
     url="https://github.com/tmick0/fragminder",
     packages=['fragminder'],
     python_requires='>=3.6',
-    install_requires=['requests>=2.25,<3', 'discord>=1.0,<2', 'aiosqlite>=0.16,<0.17', 'steam>=1.0,<2', 'csgo>=1.0,<2'],
+    install_requires=['requests>=2.25,<3', 'discord>=1.7,<1.8', 'discord.py>=1.7.3,<=1.7.3', 'aiosqlite>=0.16,<0.17', 'steam>=1.0,<2', 'csgo>=1.0,<2'],
     entry_points={"console_scripts": ["fragminder=fragminder.main:main"]}
 )
